c9328ab89cdc905cacc056df24b8e57fe767b1c7
Brandon Ashby <bash7325@gmail.com>
2021-02-07 11:03:46 -0700

    added about me, script for contact

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  //clear local storage on page load
+  localStorage.clear();
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    $(".navbar-menu").slideDown("slow", function () {
+      // Animation complete.
+    });
 
-    $(document).ready(function() {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+  //Email functions for contact
+  $("#submit-button").on("click", function (event) {
+    event.preventDefault();
+    var userEmail = $("#user-email").val().trim();
+    var userMessage = $("#user-message").val().trim();
 
-        // Check for click events on the navbar burger icon
-        $(".navbar-burger").click(function() {
-            $( ".navbar-menu" ).slideDown( "slow", function() {
-                // Animation complete.
-              });
-              
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            $(".navbar-burger").toggleClass("is-active");
-            $(".navbar-menu").toggleClass("is-active");
-            
-        });
-        
-      });
+    localStorage.setItem("user email", userEmail);
+    localStorage.setItem("user message", userMessage);
 
+    console.log(userEmail);
+    console.log(userMessage);
+    sendEmail();
 
-
-
-
-
+    function sendEmail() {
+      Email.send({
+        SecureToken: "3831b7e-5f9b-4a34-b9c2-03b23d3df736",
+        To: "bash7325@gmail.com",
+        From: userMessage,
+        Subject: "Message from my portfolio",
+        Body: userMessage,
+      }).then((message) => alert(message));
+    }
+  });
 });
